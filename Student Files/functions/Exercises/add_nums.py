acc5dcc263f32b24d0975733ad6f802eccb4fdf5
Joyce Dollenkamp <joycedol@hotmail.com>
2024-11-01 15:03:36 +0100

    added studes files + excersise 6

---
+++ Student Files/functions/Exercises/add_nums.py
@@ -0,0 +1,23 @@
+# def add_3_and_6():
+#     total = 3 + 6
+#     print('3 + 6 = ', total)
+
+# def add_10_and_12():
+#     total = 10 + 12
+#     print('10 + 12 = ', total)
+
+# def main():
+#     add_3_and_6()
+#     add_10_and_12()
+
+# main()
+
+
+def add_nums(getal1, getal2):
+    som = getal1 + getal2
+    print(som)
+
+
+add_nums(3,6)
+add_nums(1,4)
+
